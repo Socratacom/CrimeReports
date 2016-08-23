4407b15d7982ba37ea393f7738b8e4f92bede895
Michael Church <michael@fishinglounge.com>
2016-08-23 17:34:49 -0400

    progress dev

---
+++ wp-content/themes/sage-8.4.2/templates/content-homepage.php
@@ -0,0 +1,124 @@
+<section class="section-padding hero">
+	<div class="text">
+		<div class="vertical-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<h1 class="color-white text-center margin-bottom-30">Welcome to Crime Reports</h1>
+						<ul class="cta-list">
+							<li><a href="#" class="btn btn-success btn-lg">Explore the Map</a></li>						
+							<li><a href="#" class="btn btn-primary btn-lg">Agency Sign Up</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="search-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="image" style="background-image:url(/wp-content/uploads/home-hero.jpg);"></div>
+</section>
+<section class="section-padding">
+	<div class="container">
+		<div class="row no-gutters horizontal-card">
+			<div class="col thumb col-sm-6 col-md-7 col-lg-8" style="background-image:url(http://placehold.it/760x428);">		
+			</div>
+			<div class="col col-sm-6 col-md-5 col-lg-4">
+				<div class="text">
+					<h3 class="margin-bottom-15">How to use CrimeReports</h3>
+					<p>CrimeReports is the nation's largest collection of law enforcement agencies commited to transparency, public access, and citizen engagement.</p>
+				</div>
+				<div class="btn-wrapper">
+					<a href="#" class="btn btn-primary btn-block">Watch Video</a>
+				</div>			
+			</div>
+		</div>
+	</div>
+</section>
+<script>
+jQuery(function($) {
+    $('.col').matchHeight({byRow:true});
+});
+</script>
+<section class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="text-center section-title">Partner with Crime Reports</h2>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="card">
+					<div class="thumb" style="background-image:url(http://placehold.it/560x315);">
+						<a href="#"></a>
+					</div>
+					<div class="text">
+						<h5 class="margin-bottom-15">Register a Camera</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+					</div>
+					<div class="btn-wrapper">
+						<a href="#" class="btn btn-primary btn-block">Regerster Camera</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="card">
+					<div class="thumb" style="background-image:url(http://placehold.it/560x315);">
+						<a href="#"></a>
+					</div>
+					<div class="text">
+						<h5 class="margin-bottom-15">Submit a Crime Tip</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					</div>
+					<div class="btn-wrapper">
+						<a href="#" class="btn btn-primary btn-block">Submit Tip</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="card">
+					<div class="thumb" style="background-image:url(http://placehold.it/560x315);">
+						<a href="#"></a>
+					</div>
+					<div class="text">
+						<h5 class="margin-bottom-15">Law Enforcement Agency</h5>
+						<p>Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient.</p>
+					</div>
+					<div class="btn-wrapper">
+						<a href="#" class="btn btn-primary btn-block">Sign Up</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-lg-3">
+				<div class="card">
+					<div class="thumb" style="background-image:url(http://placehold.it/560x315);">
+						<a href="#"></a>
+					</div>
+					<div class="text">
+						<h5 class="margin-bottom-15">Data Researchers</h5>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Aenean eu leo quam.</p>
+					</div>
+					<div class="btn-wrapper">
+						<a href="#" class="btn btn-primary btn-block">View Data</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<script>jQuery(function(a){a(".card").matchHeight({byRow:!0})});</script>
+<section class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="text-center section-title">Browse by State</h2>
+			</div>
+			<p>query here</p>
+		</div>
+	</div>
+</section>
--- wp-content/themes/sage-8.4.2/templates/footer.php
+++ wp-content/themes/sage-8.4.2/templates/footer.php
@@ -1,5 +1,12 @@
-<footer class="content-info">
+<footer class="section-padding">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+    	<div class="col-sm-6">
+    		Motorola logo here
+    	</div>
+    	<div class="col-sm-6">
+    		socrata crap here
+    	</div>
+    </div>
   </div>
 </footer>
