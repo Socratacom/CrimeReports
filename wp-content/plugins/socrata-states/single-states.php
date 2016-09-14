5414e51d091e000446e8355d2fbc113d24090a11
mike-church <mike.church@fishinglounge.com>
2016-09-14 11:22:55 -0700

    progress dev

--- wp-content/plugins/socrata-states/single-states.php
+++ wp-content/plugins/socrata-states/single-states.php
@@ -1,7 +1,30 @@
+<section class="background-light-grey-4 masthead">
+<div class="text">
+<div class="container">
+<div class="row">
+<div class="col-sm-6">
+<h1 class="text-reverse margin-bottom-0 text-uppercase"><?php the_title(); ?></h1>
+</div>
+</div>
+</div>
+</div>
+<div class="image" style="background-image:url(/wp-content/uploads/cr-hero.jpg);"></div>
+</section>
 <section class="section-padding">
   <div class="container">
     <div class="row">
-      <h1><?php the_title(); ?></h1>
+      <div class="col-sm-12">
+          <h2 class="text-center section-title">Alabama Featured Agencies</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+          <h2 class="text-center section-title">Top Cities</h2>
+      </div>
     </div>
   </div>
 </section>
